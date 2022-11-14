a9ffb21e36df5683a90e0b3c33c19085f56590c0
RoanvDam <114396994+RoanvdamSchool@users.noreply.github.com>
2022-11-14 12:25:57 +0100

    nieuwe index

--- index.php
+++ index.php
@@ -6,9 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<?php echo "
+<?php $naam = "bob"; echo "
 <body>
-    
+    <h1>Hallo $naam</h1>
+    <h2>Wij zijn BOB</h2>
+   <footer>
+   </footer>
 </body>
 </html>
 "
